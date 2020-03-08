267b3ab055dee9909a405f701db9f80bcadb4033
Nickolas Burr <nickolasburr@gmail.com>
2020-03-07 23:51:02 -0500

    Update ConfirmCustomerByToken, GetCustomerByToken license notice

--- src/Customer/Model/ForgotPasswordToken/GetCustomerByToken.php
+++ src/Customer/Model/ForgotPasswordToken/GetCustomerByToken.php
@@ -1,18 +1,7 @@
 <?php
 /**
- * GetCustomerByToken.php
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the MIT license, which
- * is bundled with this package in the file LICENSE.txt.
- *
- * It is also available on the Internet at the following URL:
- * https://docs.auroraextensions.com/magento/extensions/2.x/backports/LICENSE.txt
- *
- * @package       AuroraExtensions_MagentoBackports
- * @copyright     Copyright (C) 2020 Aurora Extensions <support@auroraextensions.com>
- * @license       MIT
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
  */
 declare(strict_types=1);
 
